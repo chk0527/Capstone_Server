e2ad63606915123fbadcc91d73502112f02538d2
chlee <1chlee1@naver.com>
2023-04-02 22:17:15 +0900

    3

--- src/main/java/com/example/capstone_server/User.java
+++ src/main/java/com/example/capstone_server/User.java
@@ -6,7 +6,7 @@ import javax.persistence.*;
 @Table(name = "test_table")
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String name;
 
